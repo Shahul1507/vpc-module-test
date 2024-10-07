851330347df49018add0070a030d1e9a8f7029e3
shahul <shahul.met@gmil.com>
2024-10-07 17:49:24 +0530

    vpc module

--- output.tf
+++ output.tf
@@ -1,4 +1,18 @@
+#vpc
 output "vpc_id" {
   value = module.vpc.vpc_id
 
+}
+# avaliability zone
+output "az_info" {
+  value = module.vpc.az_info
+}
+
+#peering
+output "default_vpc_info" {
+  value = module.vpc.default_vpc_info
+}
+#default route table
+output "main_route_table_info" {
+  value = module.vpc.main_route_table_info
 }
