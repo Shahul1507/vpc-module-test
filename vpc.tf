0f12867d81b1a654df1c020f60f195946ddc7ab4
Shahul1507 <shahul.met@gmail.com>
2024-10-09 14:50:49 +0530

    infra

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   # source                = "../terraform-aws-vpc"
   source                = "git::https://github.com/Shahul1507/terraform-aws-vpc.git?ref=main"
-  vpc_cidr = var.vpc_cidr
+  # vpc_cidr = var.vpc_cidr
   project_name          = var.project_name
   environment           = var.environment
   common_tags           = var.common_tags
